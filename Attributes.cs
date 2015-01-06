b296e3a9ef1e7e8da2b8914b2fc2353a380c5df3
Alexandre Oliveira <rockytvbr@gmail.com>
2015-01-06 20:12:25 -0200

    Initial commit

---
+++ Attributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SettingsParser.Attributes
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public string Description;
+        public DescriptionAttribute(string Description)
+        {
+            this.Description = Description;
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Reflection;
+using SettingsParser.Attributes;
+using System.Collections.Generic;
+
+namespace SettingsParser
+{
+    public class SettingsParser<T> where T : class
+    {
+        public T Settings { get; private set; }
+        private string filePath;
+        private string baseDirectory;
+
+        /// <summary>
+        /// SettingsParser constructor.
+        /// </summary>
+        /// <param name="settings">Settings property.</param>
+        /// <param name="filePath">The path to the file SettingsParser should load/write values from/to.</param>
+        public SettingsParser(T settings, string filePath)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException("filePath");
+            }
+
+            this.Settings = settings;
+            this.filePath = filePath;
+            this.baseDirectory = Path.GetDirectoryName(filePath);
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+        }
+
+        #region Load Settings
+        /// <summary>
+        /// Loads the values from the file specified in the parser's constructor.
+        /// </summary>
+        public void LoadSettings()
+        {
+            FieldInfo[] settingFields = typeof(T).GetFields();
+
+            if (!File.Exists(filePath))
+            {
+                SaveSettings();
+            }
+            
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string currentLine = sr.ReadLine();
+                        if (String.IsNullOrEmpty(currentLine))
+                        {
+                            break;
+                        }
+
+                        string trimmedLine = currentLine.Trim();
+                        if (String.IsNullOrEmpty(trimmedLine))
+                        {
+                            continue;
+                        }
+
+                        if (!trimmedLine.Contains("=") || trimmedLine.StartsWith("#")) // If the line starts with a hashtag, skip it.
+                        {
+                            continue;
+                        }
+
+                        string currentKey = trimmedLine.Substring(0, trimmedLine.IndexOf("="));
+                        string currentValue = trimmedLine.Substring(trimmedLine.IndexOf("=") + 1);
+
+                        foreach (FieldInfo settingField in settingFields)
+                        {
+                            if (settingField.Name == currentKey)
+                            {
+                                if (settingField.FieldType == typeof(string))
+                                {
+                                    settingField.SetValue(Settings, currentValue);
+                                }
+                                if (settingField.FieldType == typeof(bool)) // We do not allow invalid values
+                                {
+                                    if (currentValue == "1" || currentValue.ToLower() == bool.TrueString.ToLower())
+                                    {
+                                        settingField.SetValue(Settings, true);
+                                    }
+                                    else if (currentValue == "0" || currentValue.ToLower() == bool.FalseString.ToLower())
+                                    {
+                                        settingField.SetValue(Settings, false);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(double))
+                                {
+                                    double doubleValue;
+                                    if (double.TryParse(currentValue, out doubleValue))
+                                    {
+                                        settingField.SetValue(Settings, doubleValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(float))
+                                {
+                                    float floatValue;
+                                    if (float.TryParse(currentValue, out floatValue))
+                                    {
+                                        settingField.SetValue(Settings, floatValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(decimal))
+                                {
+                                    decimal decimalValue;
+                                    if (decimal.TryParse(currentValue, out decimalValue))
+                                    {
+                                        settingField.SetValue(Settings, decimalValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(short))
+                                {
+                                    short shortValue;
+                                    if (short.TryParse(currentValue, out shortValue))
+                                    {
+                                        settingField.SetValue(Settings, shortValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(int))
+                                {
+                                    int intValue;
+                                    if (int.TryParse(currentValue, out intValue))
+                                    {
+                                        settingField.SetValue(Settings, intValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(long))
+                                {
+                                    long longValue;
+                                    if (long.TryParse(currentValue, out longValue))
+                                    {
+                                        settingField.SetValue(Settings, longValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(uint))
+                                {
+                                    uint uintValue;
+                                    if (uint.TryParse(currentValue, out uintValue))
+                                    {
+                                        settingField.SetValue(Settings, uintValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(ulong))
+                                {
+                                    ulong ulongValue;
+                                    if (ulong.TryParse(currentValue, out ulongValue))
+                                    {
+                                        settingField.SetValue(Settings, ulongValue);
+                                    }
+                                }
+                                if (settingField.FieldType == typeof(ushort))
+                                {
+                                    ushort ushortValue;
+                                    if (ushort.TryParse(currentValue, out ushortValue))
+                                    {
+                                        settingField.SetValue(Settings, ushortValue);
+                                    }
+                                }
+                                if (settingField.FieldType.IsEnum)
+                                {
+                                    object enumValue = Enum.Parse(settingField.FieldType, currentValue);
+                                    if (settingField.FieldType.IsEnumDefined(enumValue))
+                                    {
+                                        settingField.SetValue(Settings, enumValue);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            SaveSettings();
+        }
+        #endregion
+        #region Save Settings
+        /// <summary>
+        /// Saves the settings to a file specified in the parser's constructor.
+        /// </summary>
+        public void SaveSettings()
+        {
+            FieldInfo[] settingFields = typeof(T).GetFields();
+            if (File.Exists(filePath + ".tmp"))
+            {
+                File.Delete(filePath + ".tmp");
+            }
+            using (FileStream fs = new FileStream(filePath + ".tmp", FileMode.CreateNew))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("# Lines starting with hashtags are ignored by the reader");
+                    sw.WriteLine("# Setting file format: (key)=(value)");
+                    sw.WriteLine("");
+                    foreach (FieldInfo settingField in settingFields)
+                    {
+                        object descriptionAttribute = settingField.GetCustomAttributes(typeof(DescriptionAttribute), true).FirstOrDefault();
+                        string settingDescription = descriptionAttribute != null ? ((DescriptionAttribute)descriptionAttribute).Description : string.Empty;
+
+                        if (!string.IsNullOrEmpty(settingDescription))
+                        {
+                            sw.WriteLine(string.Format("# {0} - {1}", settingField.Name, settingDescription));
+                        }
+                        if (settingField.FieldType == typeof(string))
+                        {
+                            sw.WriteLine(string.Format("{0}={1}", settingField.Name, settingField.GetValue(Settings)));
+                        }
+                        if (settingField.FieldType == typeof(int) || settingField.FieldType == typeof(uint) || settingField.FieldType == typeof(short) || settingField.FieldType == typeof(long) || settingField.FieldType == typeof(ushort) || settingField.FieldType == typeof(ulong))
+                        {
+                            sw.WriteLine(string.Format("{0}={1}", settingField.Name, settingField.GetValue(Settings).ToString()));
+                        }
+                        if (settingField.FieldType == typeof(float) || settingField.FieldType == typeof(double) || settingField.FieldType == typeof(decimal))
+                        {
+                            sw.WriteLine(string.Format("{0}={1}", settingField.Name, settingField.GetValue(Settings).ToString()));
+                        }
+                        if (settingField.FieldType == typeof(bool))
+                        {
+                            sw.WriteLine(string.Format("{0}={1}", settingField.Name, settingField.GetValue(Settings).ToString()));
+                        }
+                        if (settingField.FieldType.IsEnum)
+                        {
+                            sw.WriteLine("# Valid values are:");
+                            foreach (int enumValue in settingField.FieldType.GetEnumValues())
+                            {
+                                sw.WriteLine(string.Format("# {0} - {1}", enumValue.ToString(), settingField.FieldType.GetEnumValues().GetValue(enumValue)));
+                            }
+                            sw.WriteLine(string.Format("{0}={1}", settingField.Name, settingField.GetValue(Settings)));
+                        }
+                    }
+                }
+            }
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            File.Move(filePath + ".tmp", filePath);
+        }
+        #endregion
+    }
+}
