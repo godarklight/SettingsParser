d8e203fb46caef41db1f9d04009ddacef9333be5
Alexandre Oliveira <rockytvbr@gmail.com>
2015-01-06 21:31:36 -0200

    Changed description in AssemblyInfo.cs

--- SettingsParser/Properties/AssemblyInfo.cs
+++ SettingsParser/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SettingsParser")]
-[assembly: AssemblyDescription("(key)=(value) parser in C#.")]
+[assembly: AssemblyDescription("Simple key equals value (key=value) parser for C#.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexandre Oliveira & Christopher Andrews")]
 [assembly: AssemblyProduct("SettingsParser")]
