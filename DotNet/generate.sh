d0cda9482528257bcce71e841dd1fc98d2180fec
Christopher Andrews <godarklight@gmail.com>
2022-01-24 15:47:16 +1000

    Switch build system to dotnet properly

--- DotNet/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find ../SettingsParser/ -iname *.cs > generate-cs.txt
-sed "/.*Compile Include.*/d" -i SettingsParser.csproj
-sed "s/^/    <Compile Include=\"/g" -i generate-cs.txt
-sed "s/$/\"\/>/g" -i generate-cs.txt
-sed "/<ItemGroup>/r generate-cs.txt" -i SettingsParser.csproj
-rm generate-cs.txt
